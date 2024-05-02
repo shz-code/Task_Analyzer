e5fb72ecff35b6faa3f0e252b17f1bea95d9712b
shz-code <web.shahidul.alam@gmail.com>
2024-05-02 18:33:50 +0600

    add: Initial Layout

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task_Analyzer.Controllers
+{
+    public class TaskController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Name"] = "Shanto";
+            return View();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -29,6 +29,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Task}/{action=Index}/{id?}");
 
 app.Run();
