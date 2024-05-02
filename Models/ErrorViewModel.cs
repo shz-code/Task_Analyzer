4f7bcd85b89bb76b70088ca92f8b437e05642e45
shz-code <web.shahidul.alam@gmail.com>
2024-05-02 14:26:12 +0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Task_Analyzer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
